5d2f08e7125d1511da520962f573c11df45b8149
Fan Lin <flin@thoughtworks.com>
2015-11-12 14:15:48 +0800

    Add more test

--- src/test/java/thoughtworks/model/CalculatorTest.java
+++ src/test/java/thoughtworks/model/CalculatorTest.java
@@ -1,24 +1,53 @@
 /*
- * Copyright 2008 Inc.
+ * Copyright 2015 ThoughtWorks Inc.
  *
- * (details of open-source license...)
+ * For demo only.
  */
 
 package thoughtworks.model;
 
-
-import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 public class CalculatorTest {
 
     @Test
-    public void shouldCalculate() {
+    public void shouldCalculateAdd() {
         Calculator calculator = new Calculator();
         calculator.setFirstNumber(1.0);
         calculator.setOperation('+');
+        calculator.setSecondNumber(2.0);
+        double result = calculator.calculate();
+        assertThat(result, is(3.0));
+    }
+
+    @Test
+    public void shouldCalculateSubtract() {
+        Calculator calculator = new Calculator();
+        calculator.setFirstNumber(3.0);
+        calculator.setOperation('-');
+        calculator.setSecondNumber(1.5);
+        double result = calculator.calculate();
+        assertThat(result, is(1.5));
+    }
+
+    @Test
+    public void shouldCalculateMultiply() {
+        Calculator calculator = new Calculator();
+        calculator.setFirstNumber(4.0);
+        calculator.setOperation('*');
+        calculator.setSecondNumber(2.0);
+        double result = calculator.calculate();
+        assertThat(result, is(8.0));
+    }
+
+    @Test
+    public void shouldCalculateDivide() {
+        Calculator calculator = new Calculator();
+        calculator.setFirstNumber(6.0);
+        calculator.setOperation('/');
         calculator.setSecondNumber(2.0);
         double result = calculator.calculate();
         assertThat(result, is(3.0));
